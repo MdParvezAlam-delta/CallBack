a41130f6920495bc338b6bbe44ddff5dcaa362a6
pa-rvez2023 <mdparvezalamalam3@gmail.com>
2026-07-11 00:08:10 +0530

    updated

--- introdunciton.js
+++ introdunciton.js
@@ -1,25 +1,25 @@
-// what is callback ?
+what is callback ?
 
-// -> CallBack is a function that passed into another function as an arguments so it can be runs later after 
-// the first task fininshes.CallBack.CallBack.
+-> CallBack is a function that passed into another function as an arguments so it can be runs later after 
+the first task fininshes.CallBack.CallBack.
 
 
-// Why we need a CallBack?
-// --> We need call back to maintain the order of the code means one step happens after another esprecially 
-// when task takes time .
+Why we need a CallBack?
+--> We need call back to maintain the order of the code means one step happens after another esprecially 
+when task takes time .
 
-// Syntax:
+Syntax:
 
-// function doWork(callback) {
-//   const result = "done";
-//   callback(result);
-// }
+function doWork(callback) {
+  const result = "done";
+  callback(result);
+}
 
-// function onDone(value) {
-//   console.log("Result:", value);
-// }
+function onDone(value) {
+  console.log("Result:", value);
+}
 
-// doWork(onDone);
+doWork(onDone);
 
-// explain: doWork(onDone) passes the function onDone into doWork as a callback.
-//  doWork runs first, then it calls onDone(result), so "Result: done" is printed.
+explain: doWork(onDone) passes the function onDone into doWork as a callback.
+ doWork runs first, then it calls onDone(result), so "Result: done" is printed.
